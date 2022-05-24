07d390876e5175e8eb241c919b64ad983ff67758
Charles de Bueger <chabekah@gmail.com>
2022-05-24 23:33:55 +0100

    Add 404 redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,12 @@
 class PagesController < ApplicationController
   def show
     @page = Page.find_by(slug: params[:slug])
+
+    # redirect_to root_url, flash: { error: 'Record not found.' } unless @page
+    # render '/public/404.html', flash: { error: 'Record not found.' } unless @page
+    # raise ActionController::RoutingError, 'Page Not Found' unless @page
+    # render file: '/public/404', status: 404, formats: [:html] unless @page
+    # render file: "#{RAILS_ROOT}/public/404.html", layout: false, status: 404 unless @page
+    render file: 'public/404.html', layout: false, status: 404 unless @page
   end
 end
