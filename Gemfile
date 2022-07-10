749fda4ae197a01e7a9612e473fd04986e9d4855
Charles de Bueger <chabekah@gmail.com>
2022-07-10 22:07:36 +0100

    Add administrate

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ gem "tailwindcss-rails", "~> 2.0"
 
 gem "devise"
 
+gem "administrate"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+    resources :events
+    resources :pages
+
+    root to: 'events#index'
+  end
   devise_for :users
+
   # home page is produced by running the home action of the PagesController
-  # with its own template
+  # with its own (home) template as the slug
   root controller: 'pages', action: 'home', slug: 'home'
 
-  # resources :events
   resources :events, only: %i[index show]
 
-  namespace :admin do
-    resources :events
-  end
+  # namespace :admin do
+  #   resources :events
+  # end
 
   # catch all route to catch any page - this could be a security risk...
   match '*slug', controller: 'pages', action: 'show', via: :get, as: 'page'
