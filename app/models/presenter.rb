146e04ef68fb01a0dbc1c2c719ad9ef2c1915c4f
Charles de Bueger <chabekah@gmail.com>
2022-12-18 00:47:25 +0000

    translate bio field for presenters

--- app/models/presenter.rb
+++ app/models/presenter.rb
@@ -3,6 +3,7 @@
 # People that present sessions. May be a candidate for STI or polymorphic
 # stuff later
 class Presenter < ApplicationRecord
+  extend Mobility
   include ActiveModel::Serialization
 
   has_rich_text :bio
@@ -10,6 +11,8 @@ class Presenter < ApplicationRecord
   has_one_attached :featured_image do |attachable|
     attachable.variant :thumb, resize_to_limit: [50, 50]
   end
+
+  translates :bio, backend: :action_text
 
   validates :name, presence: true
 end
