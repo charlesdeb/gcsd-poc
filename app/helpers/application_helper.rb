15232d1d51ba8fb0e5e82839b16864f087b2f5f5
Charles de Bueger <chabekah@gmail.com>
2023-01-03 23:19:17 +0000

    put all helpers in application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@
 
 # Helper methods
 module ApplicationHelper
+  # Drop-down for selecting locale in main nav
   def locale_select(request_path = root_path)
     select_tag 'locale',
                language_options(request_path),
@@ -9,6 +10,7 @@ module ApplicationHelper
                'x-on:change': 'window.location = $event.target.querySelector("select option:checked").dataset.url'
   end
 
+  # Drop-down for selecting timezone in main nav
   def timezone_select
     select_tag 'time-zone',
                timezone_options,
@@ -16,6 +18,22 @@ module ApplicationHelper
                'x-data': {},
                'x-title': 'Timezone Selector',
                'x-on:change': '$store.timeZoneSelect.updateTimeZone(document.querySelector(\'select#time-zone\').value)'
+  end
+
+  # Tab headers for the session types of an event
+  def session_type_tab(session_type, position)
+    bg_color_class = position.zero? ? 'bg-orange-800' : 'bg-transparent'
+    link_content = content_tag(:span, "#{session_type.name} (#{session_type.count})")
+    link_content += content_tag(
+      :span, '',
+      class: "#{bg_color_class} bg-orange-800 absolute inset-x-0 bottom-0 h-0.5",
+      'aria-hidden': true
+    )
+
+    color_class = position.zero? ? 'text-orange-900' : 'text-orange-500 hover:text-orange-700'
+
+    link_to link_content, '#',
+            class: "#{color_class} first:rounded-tl-lg last:rounded-tr-lg group relative min-w-0 flex-1 overflow-hidden bg-orange-50 py-4 px-4 text-sm font-medium text-center hover:bg-orange-200 focus:z-10"
   end
 
   private
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe ApplicationHelper, :type => :helper do # rubocop:disable Metrics/BlockLength
+RSpec.describe ApplicationHelper, :type => :helper do
   describe '#locale_select' do
     before(:each) do
       allow(I18n).to receive(:available_locales) { %i[en tr fr] }
@@ -50,6 +50,32 @@ RSpec.describe ApplicationHelper, :type => :helper do # rubocop:disable Metrics/
       expect(helper.timezone_select).to include('<option value="Asia/Singapore"')
       expect(helper.timezone_select).to include('<option value="Australia/Sydney"')
       expect(helper.timezone_select).to include('<option value="Pacific/Auckland"')
+    end
+  end
+
+  describe '#session_type_tab' do
+    let(:event) { create(:event_with_sessions) }
+    context 'first tab' do
+      subject do
+        helper.session_type_tab event.session_types_with_counts.first, 0
+      end
+
+      # TODO: seems like a very fragile test...
+      it 'is selected' do
+        expect(subject).to include('text-orange-900')
+      end
+    end
+
+    context 'other tabs' do
+      subject do
+        helper.session_type_tab event.session_types_with_counts.first, 1
+      end
+
+      # TODO: seems like a very fragile test...
+      it 'is not selected' do
+        expect(subject).to include('text-orange-500')
+        expect(subject).to include('hover:text-orange-700')
+      end
     end
   end
 end
