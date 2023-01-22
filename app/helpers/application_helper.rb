3a3b515f091e2ba18fe50f70106fe978fb226f22
Charles de Bueger <chabekah@gmail.com>
2023-01-22 22:02:11 +0000

    tidy up helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,17 +25,18 @@ module ApplicationHelper
   # Tab headers for the session types of an event
   def session_type_tab(session_type, position)
     bg_color_class = position.zero? ? 'bg-orange-800' : 'bg-transparent'
+
     link_content = content_tag(:span, "#{session_type.name} (#{session_type.count})")
+
     link_content += content_tag(
       :span, '',
-      class: "#{bg_color_class} bg-orange-800 absolute inset-x-0 bottom-0 h-0.5",
-      'aria-hidden': true
+      class: "#{bg_color_class} bg-orange-800 absolute inset-x-0 bottom-0 h-0.5", 'aria-hidden': true
     )
 
     color_class = position.zero? ? 'text-orange-900' : 'text-orange-500 hover:text-orange-700'
 
     link_to link_content, '#',
-            class: "#{color_class} first:rounded-tl-lg last:rounded-tr-lg group relative min-w-0 flex-1 overflow-hidden bg-orange-50 py-4 px-4 text-sm font-medium text-center hover:bg-orange-200 focus:z-10"
+            class: "#{color_class} first:rounded-tl-lg last:rounded-tr-lg group relative min-w-0 flex-1 overflow-hidden bg-orange-50 py-4 px-4 text-sm font-medium text-center hover:bg-orange-200 focus:z-10" # rubocop:disable Layout/LineLength
   end
 
   private
