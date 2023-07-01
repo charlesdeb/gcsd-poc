89cb2abdc8bd60d4605f980bf2b1be793366f66d
Charles de Bueger <chabekah@gmail.com>
2023-07-01 23:28:54 +0100

    fix: remove cruft from user dashboard

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -85,9 +85,6 @@ class UserDashboard < Administrate::BaseDashboard
     locale
     role
     profile_image
-    reset_password_token
-    reset_password_sent_at
-    remember_created_at
     sign_in_count
     current_sign_in_at
     last_sign_in_at
