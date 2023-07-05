0aae4cb452e0baa482d96c98595b44d7712e605c
Charles de Bueger <chabekah@gmail.com>
2023-07-05 16:16:07 +0100

    fix: sort scrolling for modals

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,9 +10,8 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin_all_from 'app/javascript/lib', under: 'lib'
 pin 'trix'
 pin '@rails/actiontext', to: 'actiontext.js'
-# pin 'alpinejs', to: 'https://ga.jspm.io/npm:alpinejs@3.10.5/dist/module.esm.js'
 pin 'alpinejs', to: 'alpinejs.js'
-pin 'luxon' # @3.1.0
-
 # Helps alpinejs work better with turbo https://github.com/SimoTod/alpine-turbo-drive-adapter
 pin 'alpine-turbo-drive-adapter', to: 'https://ga.jspm.io/npm:alpine-turbo-drive-adapter@2.0.0/dist/alpine-turbo-drive-adapter.esm.js'
+pin '@alpinejs/focus', to: '@alpinejs--focus.js' # @3.12.3
+pin 'luxon' # @3.1.0
