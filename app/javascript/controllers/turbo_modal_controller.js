24958d343f5fa791f8d07a2b8df41f1f1a2d7348
Charles de Bueger <chabekah@gmail.com>
2023-07-06 12:54:55 +0100

    fix: handle modal removal; session_type select

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -2,8 +2,41 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="turbo-modal"
 export default class extends Controller {
+  static targets = ['modal', 'content'];
   hideModal() {
-    this.element.parentElement.removeAttribute('src'); // it might be nice to also remove the modal SRC
-    this.element.remove();
+    // this.element.parentElement.removeAttribute('src'); // it might be nice to also remove the modal SRC
+    // this.element.remove();
+    console.log('hiding modal');
+    console.log({ window });
+    console.log({ document: window.document });
+
+    // Remove the styles that prevent scrolling under a modal added by alpinejs
+    // trap, since they don't seem to be being removed on real mobile devices.
+    const htmlElement = window.document.querySelector('html');
+    htmlElement.style.overflow = '';
+    htmlElement.style.paddingRight = '';
+
+    // now remove the entire modal element
+    this.modalTarget.remove();
+  }
+
+  // hide modal when clicking ESC
+  // action: "keyup@window->turbo-modal#closeWithKeyboard"
+  closeWithKeyboard(e) {
+    if (e.code == 'Escape') {
+      console.log('escape clicked');
+      this.hideModal();
+    }
+  }
+
+  // hide modal when clicking outside of modal content
+  // action: "click@window->turbo-modal#closeBackground"
+  closeBackground(e) {
+    console.log({ window });
+    if (e && this.contentTarget.contains(e.target)) {
+      return;
+    }
+
+    this.hideModal();
   }
 }
