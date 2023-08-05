b7fe5f3fdc5e166bf55bb816ac2908bf49518a3e
Charles de Bueger <chabekah@gmail.com>
2023-08-06 00:14:10 +0100

    fix: a tweak to the recaptcha stuff

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -9,14 +9,23 @@ class ContactFormController < ApplicationController
     @contact_form = ContactForm.new(contact_form_params)
     @contact_form.validate
 
-    check = verify_recaptcha action: 'signup', minimum_score: 0.7, secret_key: ENV.fetch('RECAPTCHA_SECRET_V3', nil)
+    success = verify_recaptcha(
+      action: 'contact_us', minimum_score: 0.7,
+      secret_key: ENV.fetch('RECAPTCHA_SECRET_V3', nil)
+    )
+    checkbox_success = verify_recaptcha unless success
 
-    if check && @contact_form.save
+    Rails.logger.info "recaptcha success: #{success}"
+    Rails.logger.info "recaptcha checkbox_success: #{checkbox_success}"
+
+    if (success || checkbox_success) && @contact_form.save
       NotifierMailer.with(contact_form: contact_form_params).contact_form_email.deliver_now
 
       flash[:notice] = 'Your message was sent successfully.'
       redirect_to new_contact_form_url
     else
+      @show_checkbox_recaptcha = true unless success
+      flash.now[:alert] = 'Hmm... there was a problem. Try again?'
       render :new, status: :unprocessable_entity
     end
   end
